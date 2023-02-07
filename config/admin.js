69806fdae8a76f9baabb8f5cf2e5ab811d0f7312
Richard <kasonde.nsama@gmail.com>
2023-02-07 23:45:28 +0200

    Fixing up my admin issues

--- config/admin.js
+++ config/admin.js
@@ -1,36 +1,35 @@
+const SamlStrategy = require("passport-saml").Strategy;
+const fs = require("fs");
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
+    providers: [
+      {
+        uid: "saml",
+        displayName: "Okta",
+        icon: "https://www.okta.com/sites/default/files/Okta_Logo_BrightBlue_Medium-thumbnail.png",
+        createStrategy: (strapi) =>
+          new SamlStrategy(
+            {
+              callbackURL:
+                strapi.config.server.url +
+                strapi.admin.services.passport.getStrategyCallbackURL("saml"),
+              cert: fs.readFileSync(env("SAML_CERT_PATH"), "utf-8"),
+              entryPoint: env("SAML_ENTRYPOINT_URL"),
+            },
+            (accessToken, refreshToken, profile, done) => {
+              done(null, {
+                email: profile.email,
+                username: profile.username,
+              });
+            }
+          ),
+      },
+    ],
   },
   apiToken: {
     salt: env("API_TOKEN_SALT"),
   },
   url: env("STRAPI_ADMIN_URL"),
-  admin: {
-    auth: {
-      providers: [
-        {
-          uid: "saml",
-          displayName: "Okta",
-          icon: "https://www.okta.com/sites/default/files/Okta_Logo_BrightBlue_Medium-thumbnail.png",
-          createStrategy: (strapi) =>
-            new SamlStrategy(
-              {
-                callbackURL:
-                  strapi.config.server.url +
-                  strapi.admin.services.passport.getStrategyCallbackURL("saml"),
-                cert: fs.readFileSync(env("SAML_CERT_PATH"), "utf-8"),
-                entryPoint: env("SAML_ENTRYPOINT_URL"),
-              },
-              (accessToken, refreshToken, profile, done) => {
-                done(null, {
-                  email: profile.email,
-                  username: profile.username,
-                });
-              }
-            ),
-        },
-      ],
-    },
-  },
 });

8602bbfd7c466098b0f954d959d01e37bee8ccbb
Richard <kasonde.nsama@gmail.com>
2023-02-07 23:23:03 +0200

    Added SAML stuff

--- config/admin.js
+++ config/admin.js
@@ -6,4 +6,31 @@ module.exports = ({ env }) => ({
     salt: env("API_TOKEN_SALT"),
   },
   url: env("STRAPI_ADMIN_URL"),
+  admin: {
+    auth: {
+      providers: [
+        {
+          uid: "saml",
+          displayName: "Okta",
+          icon: "https://www.okta.com/sites/default/files/Okta_Logo_BrightBlue_Medium-thumbnail.png",
+          createStrategy: (strapi) =>
+            new SamlStrategy(
+              {
+                callbackURL:
+                  strapi.config.server.url +
+                  strapi.admin.services.passport.getStrategyCallbackURL("saml"),
+                cert: fs.readFileSync(env("SAML_CERT_PATH"), "utf-8"),
+                entryPoint: env("SAML_ENTRYPOINT_URL"),
+              },
+              (accessToken, refreshToken, profile, done) => {
+                done(null, {
+                  email: profile.email,
+                  username: profile.username,
+                });
+              }
+            ),
+        },
+      ],
+    },
+  },
 });

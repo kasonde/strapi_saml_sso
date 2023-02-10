cd98de8418a161316bcb115bf526d15eaf4694bb
Richard <kasonde.nsama@gmail.com>
2023-02-10 22:51:44 +0200

    Added azure oauth2

--- config/admin.js
+++ config/admin.js
@@ -1,10 +1,38 @@
 const SamlStrategy = require("@node-saml/passport-saml").Strategy;
 const fs = require("fs");
+const jwt = require("jsonwebtoken");
 
 module.exports = ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
     providers: [
+      {
+        uid: "azure_ad_oauth2",
+        displayName: "Microsoft",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Microsoft_logo_%282012%29.svg/320px-Microsoft_logo_%282012%29.svg.png",
+        createStrategy: (strapi) =>
+          new AzureAdOAuth2Strategy(
+            {
+              clientID: env("MICROSOFT_CLIENT_ID", ""),
+              clientSecret: env("MICROSOFT_CLIENT_SECRET", ""),
+              scope: ["user:email"],
+              tenant: env("MICROSOFT_TENANT_ID", ""),
+              callbackURL:
+                strapi.admin.services.passport.getStrategyCallbackURL(
+                  "azure_ad_oauth2"
+                ),
+            },
+            (accessToken, refreshToken, params, profile, done) => {
+              let waadProfile = jwt.decode(params.id_token, "", true);
+              done(null, {
+                email: waadProfile.email,
+                username: waadProfile.email,
+                firstname: waadProfile.given_name, // optional if email and username exist
+                lastname: waadProfile.family_name, // optional if email and username exist
+              });
+            }
+          ),
+      },
       {
         uid: "saml",
         displayName: "Okta",
